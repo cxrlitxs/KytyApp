d18fa0d648213c1499593bf2efe09d610a8f4445
“Carlos <carloscasta.perea@gmail.com>
2023-09-28 20:04:16 +0200

    KytyApp class done

---
+++ lib/KytyApp.dart
@@ -0,0 +1,19 @@
+import 'package:flutter/material.dart';
+
+class KytyApp extends StatelessWidget{
+  @override
+  Widget build(BuildContext context) {
+    MaterialApp materialApp = MaterialApp(title: "Kyty",
+    routes: {
+      '/loginview' : (context) => LoginView(),
+      '/registerview' : (context) => RegisterView(),
+      '/homeview' : (context) => HomeView(),
+      '/splashview' : (context) => SplashView(),
+    },
+      initialRoute: '/loginview',
+    );
+
+    return materialApp;
+
+  }
+}

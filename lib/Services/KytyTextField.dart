0da1197cca63dbff8c58b515a3951039b7f810f0
“Carlos <carloscasta.perea@gmail.com>
2023-10-02 20:33:13 +0200

    KytyTextField TextField Structure Done

--- lib/Services/KytyTextField.dart
+++ lib/Services/KytyTextField.dart
@@ -13,4 +13,32 @@ class KytyTextField extends StatelessWidget{
     this.dPaddingV=15
   }) : super(key: key);
 
+  @override
+  Widget build(BuildContext context) {
+    // TODO: implement build
+
+    return Padding(padding: EdgeInsets.symmetric(horizontal: dPaddingH, vertical: dPaddingV),
+      child: Row(children: [
+        Image.asset("resources/logo_kyty2.png",width: 50, height: 50),
+        Flexible(
+          child: TextFormField(
+              controller: tecController,
+              obscureText: blIsPassword,
+              enableSuggestions: !blIsPassword,
+              autocorrect: !blIsPassword,
+              decoration: InputDecoration(
+                border: OutlineInputBorder(),
+                //hintText: sHint,
+                fillColor: Colors.greenAccent,
+                filled: true,
+                labelText: sHint,
+                suffixIcon:Icon(Icons.check_circle),
+                prefixIcon: Icon(Icons.check_circle),
+              )),
+        ),
+        //Image.network("https://media.tenor.com/zBc1XhcbTSoAAAAC/nyan-cat-rainbow.gif",width: 80, height:80),
+      ],
+      ),
+    );
+
 }

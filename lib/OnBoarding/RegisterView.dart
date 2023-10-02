11a406cfc8785f25f51a688e55d263d69eac0ea9
“Carlos <carloscasta.perea@gmail.com>
2023-10-02 20:14:34 +0200

    RegisterView Structure Done

--- lib/OnBoarding/RegisterView.dart
+++ lib/OnBoarding/RegisterView.dart
@@ -1,3 +1,70 @@
-class RegisterView{
+import 'package:firebase_auth/firebase_auth.dart';
+import 'package:flutter/material.dart';
+
+class RegisterView extends StatelessWidget{
+
+  //Methods & Variables
+
+  late BuildContext _context;
+
+  TextEditingController usernameController=TextEditingController();
+  TextEditingController passwordController=TextEditingController();
+  TextEditingController respassController=TextEditingController();
+
+  SnackBar snackBar = SnackBar(
+    content: Text('Yay! A SnackBar!'),
+  );
+
+  void onClickCancelar(){
+    Navigator.of(_context).pushNamed("/loginview");
+  }
+  void onClickAceptar() async {
+    //print("DEBUG>>>> "+usernameController.text);
+    if(passwordController.text==respassController.text) {
+      try {
+        await FirebaseAuth.instance.createUserWithEmailAndPassword(
+          email: usernameController.text,
+          password: passwordController.text,
+        );
+
+        Navigator.of(_context).pushNamed("/loginview");
+
+      } on FirebaseAuthException catch (e) {
+        if (e.code == 'weak-password') {
+          print('The password provided is too weak.');
+        } else if (e.code == 'email-already-in-use') {
+          print('The account already exists for that email.');
+        }
+      } catch (e) {
+        print(e);
+      }
+    }
+    else{
+      ScaffoldMessenger.of(_context).showSnackBar(snackBar);
+    }
+  }
+
+  @override
+  Widget build(BuildContext context) {
+    // TODO: implement build
+    _context=context;
+
+    Column columna = Column(children: [
+      Text("Bienvenido al registro de Kyty",style: TextStyle(fontSize: 25)), ],);
+
+    AppBar appBar = AppBar(
+      title: const Text('Register'),
+      centerTitle: true,
+      shadowColor: Colors.pink,
+      backgroundColor: Colors.greenAccent,
+    );
+
+    Scaffold scaf=Scaffold(body: columna,
+      //backgroundColor: Colors.deepOrange,
+      appBar: appBar,);
+
+    return scaf;
+
+  }
 
 }

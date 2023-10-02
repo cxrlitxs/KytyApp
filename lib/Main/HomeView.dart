7b2943e7adfc04b2b23d170eabe9f7e4a11de471
“Carlos <carloscasta.perea@gmail.com>
2023-10-02 21:01:32 +0200

    HomeView with text

--- lib/Main/HomeView.dart
+++ lib/Main/HomeView.dart
@@ -1,3 +1,9 @@
 class HomeView extends StatelessWidget{
 
+  @override
+  Widget build(BuildContext context) {
+    // TODO: implement build
+    return Text("ESTAMOS TRABAJANDO EN ELLO...");
+  }
+
 }
